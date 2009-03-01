b825c1d17ffee394f9e96cb76161b6c23d90c90d
saasmath <saasmath@63ea7b38-023a-11de-a3ef-1f8437fb274a>
2009-03-01 02:54:00 +0000

    Localized all strings bugID=1

--- trunk/memorybank/memorybankstatus.php
+++ trunk/memorybank/memorybankstatus.php
@@ -1,18 +1,21 @@
 <?php
-require_once('config.php');
+require_once ('../../config.php');
+require_once ('locallib.php');
+global $CFG;
 $username = optional_param('username');
-$user = get_record('user','username',$username);
-if(empty($user))
-{
-	echo("$username is not setup for using Memory Banks");
+$user = get_record('user', 'username', $username);
+if (empty($user)){
+	echo (get_string('no_such_user', 'memorybank', $username));
+	//"$username is not setup for using MemoryBanks");
 	exit;
 }
 $currenttime = time();
-$qcount = count_records_select('memorybank_schedule',"userid = {$user->id} AND nextviewing <= {$currenttime}");
-if(empty($qcount))
-{
-	echo("Memorybanks are up-to-date for $username.  Check back after 3PM");
+$qcount = count_records_select('memorybank_schedule', "userid = {$user->id} AND nextviewing <= {$currenttime}");
+if (empty($qcount)){
+	
+	echo (get_string('questions_completed', 'memorybank', userdate(nextViewingDay(),'%H:%M')));
 	exit;
+	
 }
-echo("<a href='http://moodle2.seattleacademy.org/mod/memorybank/view.php' target='_blank'>$qcount questions for $username.  Click to proceed</a>");
+echo ("<a href='$CFG->wwwroot/mod/memorybank/view.php' target='_blank'>$qcount" . get_string("userQcount", "memorybank", $username) . "</a>");
 ?>
